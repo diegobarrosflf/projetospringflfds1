81cc817e36e310f3018b3704b155477bf98efb93
diegobarros <diegobarrosflf@gmail.com>
2021-06-02 20:47:15 -0300

    Criar o projeot Spring

---
+++ src/main/java/com/example/demo/Projetospringflfds1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetospringflfds1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetospringflfds1Application.class, args);
+	}
+
+}
